43ddc2d631566168fff12362b57a53d54123f014
DanielMPang <114116375+DanielMPang@users.noreply.github.com>
2025-03-08 21:29:26 -0600

    Add files via upload

--- employees.py
+++ employees.py
@@ -2,7 +2,7 @@
 Student information for this assignment:
 
 Replace <FULL NAME> with your name.
-On my/our honor, <Daniel Pang> and <FULL NAME>, this
+On my/our honor, Daniel Pang and <FULL NAME>, this
 programming assignment is my own work and I have not provided this code to
 any other student.
 
